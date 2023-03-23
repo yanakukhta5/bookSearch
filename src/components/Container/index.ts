0a301a7ca54a62973c70a77c2e64545928d00a05
Yana Kukhta <kukhta0school@gmail.com>
2023-03-23 12:17:09 +0300

    add Container

---
+++ src/components/Container/index.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  padding: 0px 20px;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  position: relative;
+`
