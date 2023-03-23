521e6c105a4b3920be1478d6cc4bb45802da0df0
Yana Kukhta <kukhta0school@gmail.com>
2023-03-23 16:41:16 +0300

    fix search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,11 +3,11 @@ import { useState, FC, ChangeEvent } from 'react'
 import { SearchProps } from './types'
 import { Wrapper, Submit, Query } from './style'
 
-export const Search: FC<SearchProps> = function ({ placeholder, value, onInputFunc, onSubmit }) {  
+export const Search: FC<SearchProps> = function ({ placeholder, value, onInputFunc, onSubmit, disabled }) {
   return (
     <Wrapper onSubmit={onSubmit}>
       <Query onInput={onInputFunc} type="search" placeholder={placeholder} value={value} />
-      <Submit fullwidth={false} color="secondary" background="primary" type="submit"></Submit>
+      <Submit disabled={disabled} fullwidth={false} color="secondary" background="primary" type="submit"></Submit>
     </Wrapper>
   )
 }
