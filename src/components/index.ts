e4976cf16ed4caabceebf14711c9133b3c53d221
Yana Kukhta <kukhta0school@gmail.com>
2023-03-23 12:18:57 +0300

    add index components file

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { Button } from './Button'
+
+export { Container } from './Container'
+
+// export { Dropdown } from './Dropdown'
+
+export { Input } from './Input'
+
+export { Search } from './Search'
