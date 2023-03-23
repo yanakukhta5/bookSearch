2c08073c6ddf4cf1c93b67865c4ecc0df27945b2
Yana Kukhta <kukhta0school@gmail.com>
2023-03-23 12:17:45 +0300

    add input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,14 @@
+import { useState, FC, ChangeEvent } from 'react'
+
+import { InputProps } from './types'
+import { Wrapper } from './style'
+
+export const Input: FC<InputProps> = function ({placeholder, value, onInput}) {
+  return (
+    <Wrapper onInput={onInput} placeholder={placeholder} value={value}/>
+  )
+}
+
+Input.defaultProps = {
+  type: 'text'
+}
